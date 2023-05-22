ee9581537a98195328a5876d6d8de54ba52b3b86
Davie Kibet <118133684+daviewisdm@users.noreply.github.com>
2023-05-22 09:22:45 +0300

    Add files via upload

---
+++ one.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int number;
+    char name[50];
+
+
+    scanf("Enter number = %d \n",&number);
+    printf("Number is", name);
+    scanf("Enter name = %c \n", &name);
+    printf("Name is", name);
+    return 0;
+
+}
